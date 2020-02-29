b804bfc190040fbd739ffb9d665e60a6c5e7cd3f
vinay <vinay.hpatil@gmail.com>
2020-02-29 10:36:19 +0530

    added build instructions and fixed some minor build issues

---
+++ linux/tlpi-book/howtobuild.md
@@ -0,0 +1,3 @@
+
+
+sudo apt-get install libcap-dev
--- linux/tlpi-book/sockets/scm_rights.h
+++ linux/tlpi-book/sockets/scm_rights.h
@@ -13,6 +13,7 @@
 #include <sys/socket.h>
 #include <sys/un.h>
 #include <fcntl.h>
+#include <stdbool.h>
 #include "unix_sockets.h"       /* Declares our unix*() socket functions */
 #include "tlpi_hdr.h"
 
